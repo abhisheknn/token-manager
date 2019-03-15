57a18a2ee87cb71f6cb4209974d3cb0512eca8cb
Nagpure <abhisheknagpure08@gmail.com>
2019-03-15 14:15:49 +0530

    add nodes to cassandra connector

--- src/main/java/com/micro/auth/cassandra/CassandraConnector.java
+++ src/main/java/com/micro/auth/cassandra/CassandraConnector.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Component;
 
 import com.micro.auth.constant.AppConstants;
 
-
 @Component
 public class CassandraConnector extends com.micro.cassandra.CassandraConnector {
 	CassandraConnector(){
-	super.connect(AppConstants.CASSANDRA_HOST, Integer.parseInt(AppConstants.CASSANDRA_PORT));	
+		String[] nodes=AppConstants.CASSANDRA_HOST.split(",");
+	super.connect(nodes, Integer.parseInt(AppConstants.CASSANDRA_PORT));	
 	}
 }
