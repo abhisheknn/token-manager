dfa78a1f5cf9b0a05bbb58a1d9ffa66fe0546bd7
Abhishek Nagpure <abhisheknagpure08@gmail.com>
2021-09-19 23:07:28 +0530

    Move the DTOs to common

---
+++ token-manager-common/src/main/java/com/micro/auth/pojo/Role.java
@@ -0,0 +1,16 @@
+package com.micro.auth.pojo;
+
+public class Role {
+
+	private String[] role;
+
+	public String[] getRole() {
+		return role;
+	}
+
+	public void setRole(String[] role) {
+		this.role = role;
+	}
+	
+	
+}
