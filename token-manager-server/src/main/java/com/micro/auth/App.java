9984599f5630586b463686d2fc3badf4578138db
Abhishek Nagpure <abhisheknagpure08@gmail.com>
2021-09-20 17:49:41 +0530

    Add eureka service discovery

--- token-manager-server/src/main/java/com/micro/auth/App.java
+++ token-manager-server/src/main/java/com/micro/auth/App.java
@@ -2,11 +2,12 @@ package com.micro.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-public class App 
+@EnableEurekaClient
+public class App
 {
 	  public static void main(String[] args) {
 		    SpringApplication.run(App.class, args);
