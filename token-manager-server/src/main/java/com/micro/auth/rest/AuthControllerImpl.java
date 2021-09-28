fea24057a2c655fe43983c1c39536da8f0699f1d
Abhishek Nagpure <abhisheknagpure08@gmail.com>
2021-09-28 20:25:33 +0530

    Change for get public key

--- token-manager-server/src/main/java/com/micro/auth/rest/AuthControllerImpl.java
+++ token-manager-server/src/main/java/com/micro/auth/rest/AuthControllerImpl.java
@@ -1,17 +1,17 @@
 package com.micro.auth.rest;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.datastax.driver.mapping.annotations.QueryParameters;
 import com.micro.auth.pojo.Machine;
 import com.micro.auth.pojo.Tenant;
 import com.micro.auth.services.AuthService;
@@ -56,9 +55,11 @@ public class AuthControllerImpl implements AuthController {
 	}
 
 
-	@RequestMapping(value="/getPublicKey",method = RequestMethod.GET,consumes=MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<String> getPublicKey() {
-		return ResponseEntity.ok(authService.getPublicKey());
+	@RequestMapping(value="/getPublicKey",method = RequestMethod.GET)
+  public Map<String,String> getPublicKey() {
+    Map map=new HashMap();
+    map.put("publicKey",authService.getPublicKey());
+		return map;
 	}
 
 	@Override
